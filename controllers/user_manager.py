38502741dbb1388aaa1b77c50ff6f51229822e33
Facundo-Acosta-dv <facundo.acosta.dv@gmail.com>
2024-09-21 16:19:23 -0300

    Added menu system and other improvements

--- controllers/user_manager.py
+++ controllers/user_manager.py
@@ -1,16 +1,13 @@
 # person_manager.py
-from models.user import User
 from controllers.controllers import Controller
+from models.user import User
+from utils import debug_log
 import os, json
 
 controller = Controller()
 
 class UserManager:
 
-    # Initializes constructor.
-    def __init__(self):
-        pass
-    
     # Loads "users.json".
     def load_users(self): 
         # Asks to create "users.json" if it doesn't exists and returns an empty list.
@@ -18,7 +15,7 @@ class UserManager:
             # Checks for the file "users.json".
             if not os.path.exists("users.json"):
                 if controller.yes_or_no("'users.json' doesn't exist, do you want to create it?: ", "Please, awnser with 'Yes' or 'No': "):
-                    controller.debug_log("creating 'users.json'...")
+                    debug_log("creating 'users.json'...")
                     controller.create_json("users.json", [])
                     return []
                 else:
@@ -30,7 +27,7 @@ class UserManager:
                         users_file = json.loads(content)
                         return users_file
                     else:
-                        controller.debug_log("'users.json' is empty or contains only whitespaces, returning [].")
+                        debug_log("'users.json' is empty or contains only whitespaces, returning [].")
                         return []
         except Exception as error:
             print(f"An unexpected error ocurred: {error}")
@@ -42,16 +39,16 @@ class UserManager:
             
     # Saves "users.json" file.
     def save_users(self, users):
-        controller.debug_log(users)
+        debug_log(users)
         with open("users.json", "w") as file:
             json.dump(users, file, indent=4)
-            controller.debug_log("'users.json' overwritten! ")
+            debug_log("'users.json' overwritten! ")
         
     # Adds a user to the list.
     def add_user(self, users_file, new_user):
-        controller.debug_log("Start of function add_user... ")
-        controller.debug_log(f"users_file --> {users_file}")
-        controller.debug_log(f"new_user--> {new_user.__dict__}")
+        debug_log("Start of function add_user... ")
+        debug_log(f"users_file --> {users_file}")
+        debug_log(f"new_user --> {new_user.__dict__}")
         users_file.append(new_user.__dict__)
         self.save_users(users_file)
          
@@ -66,19 +63,19 @@ class UserManager:
             'time_created': str(controller.get_time)
         }
         filename = f'user_{user.user_id}_data.json'
-        controller.debug_log(f"user data:\n{user_data}")
+        debug_log(f"user data:\n{user_data}")
         with open(filename, 'w') as file:
             json.dump(user_data, file, indent=4)
-            controller.debug_log("user saved successfully")
+            debug_log("user saved successfully")
     
     # Loads an user from "users.json".
     def load_user(cls, user_id):
         users = cls.load_users()
         for _, user in enumerate(users): # Iterates users.json in search of an user with the ID provided.
             if user.get("id") == str(user_id):
-                controller.debug_log(user)
-                controller.debug_log(f"user {user.get('username')} found with id {user_id}")
-                controller.debug_log(f"returning user...")
+                debug_log(user)
+                debug_log(f"user {user.get('username')} found with id {user_id}")
+                debug_log(f"returning user...")
                 return user # Returns user dict and the index in "users.json" file.
         print("User not found! ")
     
@@ -86,7 +83,7 @@ class UserManager:
     def create_user(self):
         users_file = self.load_users()
         if users_file == False:
-            controller.debug_log("User refused to create 'users.json' file")
+            debug_log("User refused to create 'users.json' file")
             print("Exiting...")
             return False
         else:     
--- views/view.py
+++ views/view.py
@@ -1,25 +1,30 @@
-from models.user import User
+from controllers.controllers import Controller
 from controllers.user_manager import UserManager
+from utils import debug_log
+from views.terminal import Terminal
 import os, json
 
 manager = UserManager()
+controller = Controller()
+terminal = Terminal()
 author = "hi7nn"
 
-class TaskView:
+class View:
 
-    # TO-DO fix this fucking shit
+    # I may deprecate this.
     def show_person(self, user):
         if user in manager.users:
             print(f"The user username is {user.username}")
         else:
             print("The user doesn't exist")
     
-    # List of all persons
+    # This too.
     def user_list(self):
         print(manager.users)
         for user in manager.users:
             print(user.age)
 
+    # This too, too.
     def display_user_info(self, user_id):
         filename = f'user_{user_id}_data.json'
         if os.path.exists(filename):
@@ -30,4 +35,27 @@ class TaskView:
                         print("• No tasks found!")
                     else:
                         print(f"• {key.capitalize()}: {value}")
+
+    # TO-DO MENU HEADERS AND OPTIONS PRINT SEPARATELY.
+    def main_menu_header(self, header_text=""):
+        if header_text: # If a header is given, prints it.
+            header_text += "\n"
+        return (f"Main menu\n{header_text}1.Add user - 2.Load user - 3.List users")
+
+
+    # Displays main menu on terminal.
+    def main_menu(self):
+        terminal.clear_console()
+        # Main menu options variables.
+        min_options = 1
+        max_options = 4
+        out_of_range_text = ""
+        value_error_text = ""
+        prompt_text = f"What do you wanna do?: "
+        
+        debug_log(controller.menu_input(min_options, max_options, prompt_text, out_of_range_text, value_error_text))
+        
+            
+    
+
             
